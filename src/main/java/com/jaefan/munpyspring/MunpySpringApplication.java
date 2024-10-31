d348d6700717e89f82f0e08bff9b62942cb109c4
유승범 <yuseungbeom@yuseungbeom-ui-MacBookAir.local>
2024-10-31 14:53:58 +0900

    chore: 스프링 프로젝트 초기화

---
+++ src/main/java/com/jaefan/munpyspring/MunpySpringApplication.java
@@ -0,0 +1,13 @@
+package com.jaefan.munpyspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MunpySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MunpySpringApplication.class, args);
+	}
+
+}
