5208e80306a4917d44101063a84a1b667d4d388f
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2025-03-01 04:59:10 +0900

    fix: 우분투 환경에 맞게 크롬 환경 설정 수정

--- src/main/java/com/jaefan/munpyspring/common/config/AppConfig.java
+++ src/main/java/com/jaefan/munpyspring/common/config/AppConfig.java
@@ -2,6 +2,7 @@ package com.jaefan.munpyspring.common.config;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,10 +16,22 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AppConfig {
 
+	/**
+	 * 현재 인스턴스에서 크롬을 실행하려면
+	 * 컨테이너나 서버 환경에서는 GUI가 없으므로, Chrome을 headless 모드로 실행해야 함
+	 * 작성자 : 냄규
+	 */
 	@Bean
 	public WebDriver webDriver() {
 		WebDriverManager.chromedriver().browserVersion("130.0.6723.92").setup();
-		return new ChromeDriver();
+
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless");             // GUI 없이 실행
+		options.addArguments("--disable-gpu");          // GPU 사용 비활성화
+		options.addArguments("--no-sandbox");           // 샌드박스 모드 비활성화
+		options.addArguments("--disable-dev-shm-usage");  // /dev/shm 사용 제한 문제 해결
+
+		return new ChromeDriver(options);
 	}
 
 	@Bean
