38dc5794e8f559377226180e1c8d982e909f87a6
HuttTheJAVA <chlghksdyd31@gmail.com>
2025-04-01 15:50:52 +0900

    feat: 실종 동물 등록 시 품종 및 보호소를 매핑하는 코드 추가
    - SecurityContextHolder에 담긴 인증 객체로 현재 실종 동물 등록 요청한 보호소 식별 및 실종 동물의 보호소로 매핑
    - 실종 동물 dto의 breedName을 BreedRepository에서 찾아 실종 동물의 품종으로 매핑

--- src/main/java/com/jaefan/munpyspring/shelter/domain/repository/ShelterRepository.java
+++ src/main/java/com/jaefan/munpyspring/shelter/domain/repository/ShelterRepository.java
@@ -5,10 +5,13 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jaefan.munpyspring.shelter.domain.model.Shelter;
+import com.jaefan.munpyspring.user.domain.model.User;
 
 public interface ShelterRepository extends JpaRepository<Shelter, Long>, ShelterRepositoryCustom {
 
 	Optional<Shelter> findByName(String name);
+
+	Optional<Shelter> findByUser(User user);
 
 	Optional<Shelter> findByNameAndTelNo(String name, String telno);
 }
